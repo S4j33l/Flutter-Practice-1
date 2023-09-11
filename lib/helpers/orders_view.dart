60724f9f512bf8e264a1361d01de24ded406b67b
Sajeel <sajeeltipu4903@gmail.com>
2023-09-11 16:00:37 +0500

    orders

--- lib/helpers/orders_view.dart
+++ lib/helpers/orders_view.dart
@@ -1,3 +1,32 @@
-class Orders{
-  
+class Orders {
+  static List<String> orders = [
+    "Samsung",
+    "Oppo",
+    "Apple",
+    "Vivo",
+    "OnePlus",
+    "Huawei",
+    "Nokia",
+    "Infinix",
+    "Alcatel",
+    "Asus",
+    "Sony",
+    "HTC",
+    "Tecno"
+  ];
+  static List<int> orderPrices = [
+    1239,
+    2323,
+    7839,
+    8921,
+    9992,
+    7384,
+    8389,
+    2923,
+    5555,
+    3987,
+    4675,
+    2839,
+    8973,
+  ];
 }
--- lib/routes/fifth_page.dart
+++ lib/routes/fifth_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:internship_application_1/helpers/orders_view.dart';
 
 class FifthPage extends StatelessWidget {
   const FifthPage({super.key});
@@ -10,7 +11,52 @@ class FifthPage extends StatelessWidget {
         title: const Text("Orders"),
         centerTitle: true,
       ),
-      body: ListView(),
+      body: ListView.builder(
+        itemCount: Orders.orders.length,
+        itemBuilder: (context, index) => Container(
+          color: Colors.cyan[800],
+          child: ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+            child: Card(
+              elevation: 0.0,
+              margin: const EdgeInsets.all(5.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      const Icon(Icons.shopping_bag_rounded),
+                      Column(
+                        children: <Widget>[
+                          Text(Orders.orders[index],
+                              style: Theme.of(context).textTheme.displayMedium),
+                          Text("Price: ${Orders.orderPrices[index]}")
+                        ],
+                      ),
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: Text("Deliver?",
+                              style: Theme.of(context).textTheme.displayMedium)),
+                    ],
+                  ),
+                  const Divider(indent: 35.0, endIndent: 35.0, thickness: 2.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text(DateTime.now().toString()),
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: Text("Details",
+                              style: Theme.of(context).textTheme.displayMedium)),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        physics: const BouncingScrollPhysics(),
+      ),
     );
   }
 }
